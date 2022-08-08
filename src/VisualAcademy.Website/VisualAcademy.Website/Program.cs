29fb18f579c1ed9b8ebd7f66eef089f1b1cee61d
Park YongJune <redplus@redplus.net>
2022-08-09 08:54:22 +0900

    Minimal APIs

--- src/VisualAcademy.Website/VisualAcademy.Website/Program.cs
+++ src/VisualAcademy.Website/VisualAcademy.Website/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "¾È³çÇÏ¼¼¿ä!");
+// Minimal APIs
+app.MapGet("/", () => "Hello, World!");
+app.MapGet("/hello", () => new { Hello = "World" }).WithName("HelloWorldApi");
+app.MapGet("/goodbye", () => new { Goodbye = "World" }).WithTags("VisualAcademyDemos");
+app.MapGet("/version", () => "1.0");
+app.MapGet("/creator", () => "https://github.com/VisualAcademy");
 
 app.Run();
