ae8ce6cae02a6efd9f49cb6435489b677d271b45
Park YongJune <redplus@redplus.net>
2022-08-10 09:04:06 +0900

    UseStaticFiles 미들웨어 추가

--- src/VisualAcademy.Website/VisualAcademy.Website/Program.cs
+++ src/VisualAcademy.Website/VisualAcademy.Website/Program.cs
@@ -1,11 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+// 미들웨어 추가 
+app.UseStaticFiles(); // 정적인 HTML, CSS, JavaScript, ... 실행
+
+
 // Minimal APIs
 app.MapGet("/", () => "Hello, World!");
 app.MapGet("/hello", () => new { Hello = "World" }).WithName("HelloWorldApi");
 app.MapGet("/goodbye", () => new { Goodbye = "World" }).WithTags("VisualAcademyDemos");
 app.MapGet("/version", () => "1.0");
 app.MapGet("/creator", () => "https://github.com/VisualAcademy");
+
+
 
 app.Run();
